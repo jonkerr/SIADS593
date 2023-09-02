6cf283a4e762b8eae0aff63061c35ab192558cc9
Jonathan Kerr <jjzek@umich.edu>
2023-09-02 11:12:54 -0700

    Download utilization data

--- Makefile
+++ Makefile
@@ -3,3 +3,14 @@ all: raw_data/insulin_pricing.csv
 raw_data/insulin_pricing.csv:
 	python get_data.py
 
+# clean the generated artificats
+clean:
+	rm artifacts/*.csv
+
+# if for some reason we want to also clean the downloaded/source data, we can use the cleanraw target
+# generally not recommended as these files are huge and we shouldn't plan on editing the source
+cleanraw:
+	rm raw_data/*.csv
+
+# remove all data
+cleanall: cleanraw clean
--- get_data.py
+++ get_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
+
 
 def download_pricing(outfile='raw_data/insulin_pricing.csv'):
     # doesn't really do anything yet
@@ -7,5 +9,18 @@ def download_pricing(outfile='raw_data/insulin_pricing.csv'):
     df.to_csv(outfile)
 
 
+def download_drug_utiliztion(outfile='raw_data/drug_utilization_2021.csv'):
+    url = 'https://data.medicaid.gov/api/1/datastore/query/eec7fbe6-c4c4-5915-b3d0-be5828ef4e9d/0/download'
+    params = {'format': 'csv'}
+    response = requests.get(url, params=params)
+    with open(outfile, mode="wb") as file:
+        file.write(response.content)
+
+
+def download_all():
+    download_drug_utiliztion()
+    # download_pricing()
+
+
 if __name__ == '__main__':
-    download_pricing()
+    download_all()
